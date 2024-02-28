4be2acdc612a364f901e780eff4d0011743a8a27
Raphyah <leonardosolimenoluiz@gmail.com>
2024-02-28 05:22:21 -0300

    Split `DataStorage.prototype.save` and made changes to loops.
    Now `DataStorage.prototype.save(force)` is now divided into 2 individual methods, `DataStorage.prototype.save()` and `DataStorage.prototype.saveAsync()`.
    Now loops will return a promise, and to break a loop a new instance of `BreakLoop` must be passed to it.
    Renamed `ObjectNotation` to `NotationHandler` and instead of an anonymous function it is now a constructor.
    Small changes to the rest of the code.

--- build/DataStorage.js
+++ build/DataStorage.js
@@ -10,16 +10,15 @@
  * Instead of assigning the values directly to the dynamic properties, it should first store the data to a custom Map object called CloneableMap.
  *
  * Examples can be found on my GitHub.
- * 
+ *
  * Release date: January 25, 2024
- * Last updated: January 27, 2024
+ * Last updated: February 28, 2024
  */
 'use strict';
 
 /**
- * the `world` instance
- * - used to set dataStorage to it
  * the `World` constructor
+ * - used to set dataStorage to it
  * - used to check if a certain value passed is instance of it
  * the `Entity` constructor
  * - used to append data directly to it's prototype
@@ -29,38 +28,42 @@
  * - used to append data directly to it's prototype
  * - used to check if a certain value passed is instance of it
  */
-import { system, world, Entity, ItemStack } from '@minecraft/server';
+import { system, World, Entity, ItemStack } from '@minecraft/server';
+
+function BreakLoop (reason, status) {
+  this.reason = reason;
+  this.exitStatus = status;
+}
 
 const smoothLoop = (callback, map) => new Promise(resolve => {
   let iterable = map.entries();
-  
+
+  let lastReturned;
+
   function runLoop() {
-    const response = [];
-    const pair = iterable.next().value;
-    if (pair) {
-      response.push(pair[1], pair[0]);
+    if (lastReturned && lastReturned.constructor !== BreakLoop) {
+      const pair = iterable.next().value;
+
+      if (pair) {
+        lastReturned = callback(...pair);
+      } else {
+        lastReturned = new BreakLoop('Iteration finished', 0);
+      }
     }
-    
-    let continueLoop;
-    if (response.length > 0) {
-      continueLoop = callback(...response);
-    } else {
-      continueLoop = false;
-    }
-    
-    if (continueLoop !== false) {
+
+    if (lastReturned && lastReturned.constructor !== BreakLoop) {
       system.run(runLoop);
     } else {
-      resolve();
+      resolve(lastReturned);
     }
   }
-  
+
   system.run(runLoop);
 });
 
 /**
  * Constructor used to define compressors
- * 
+ *
  * [Static properties]
  * - Compressor.default
  *   - Set or get default compressor
@@ -88,7 +91,7 @@ const smoothLoop = (callback, map) => new Promise(resolve => {
  */
 export class Compressor {
   static #list = [];
-  
+
   #name;
   #compress;
   #decompress;
@@ -142,7 +145,7 @@ export class Compressor {
    * @returns {*} The decompressed data
    */
   decompress(data) { return this.#decompress(data) }
-  
+
   /**
    * Adds a compressor to the list
    * @param {Compressor} compressor The compressor to be added
@@ -170,15 +173,15 @@ export class Compressor {
    */
   static find(value = this.default) {
     const compressor = value?.constructor === Compressor ? value : this.#list.find(x => new RegExp(value).test(x.name));
-    
+
     return compressor;
   }
-  
+
   /**
    * The default compressor to be used
    */
   static #default;
-  
+
   /**
    * Sets the default compressor to be used
    * @param {Compressor} value The compressor to be used
@@ -207,48 +210,101 @@ Compressor.default = NO_COMPRESSION;
  * Default is JSON.
  *
  * Example:
- * ObjectNotation.default = JSON5;
+ * NotationHandler.default = JSON5;
  */
-export const ObjectNotation = (function() {
-  let defaultON;
-  
-  const obj = {};
-  
-  Object.defineProperty(obj, 'default', {
-    get: function() {
-      return defaultON;
-    },
-    set: function(value) {
-      if (value?.constructor !== Object) {
-        throw new TypeError(`Default ObjectNotation should be of type Object, but instead ${value?.constructor.name} was found`);
-      }
-      if (value.parse?.constructor !== Function || value.stringify?.constructor !== Function) {
-        throw new SyntaxError(`Invalid ObjectNotation was found. parse/stringify functions are missing`);
-      }
-      defaultON = value;
-    },
-  });
-  
-  obj.default = JSON;
-  
-  return obj;
-})();
+export class NotationHandler {
+  static #list = [];
+  #name;
+  #parse;
+  #stringify;
+  constructor(notationName) {
+    if (notationName?.constructor !== String) {
+      throw new TypeError(`The 'notationName' parameter of 'NotationHandler' should be of type String, but instead ${notationName?.constructor.name} was found`);
+    }
+    this.#name = notationName;
+    this.parseMethod = function(data) {
+      return data;
+    }
+    this.stringifyMethod = function(data) {
+      return data;
+    }
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set parseMethod(value) {
+    this.#parse = value;
+  }
+
+  set stringifyMethod(value) {
+    this.#stringify = value;
+  }
+
+  parse(...args) {
+    return this.#parse(...args);
+  }
+
+  stringify(...args) {
+    return this.#stringify(...args);
+  }
+
+  static add(notationHandler) {
+    if (notationHandler.constructor !== Compressor) {
+      throw new TypeError(`Trying to add invalid notation handler to Compressor.add(compressor)`);
+    }
+    if (this.#list.includes(notationHandler)) {
+      console.warn(`NotationHandler with name ${compressor.name} was not added due to already existing inside the list`);
+      return false;
+    }
+    if (this.find(notationHandler.name)) {
+      console.warn(`NotationHandler with name ${compressor.name} could not be added to the list due to the name being already in use`);
+      return false;
+    }
+    this.#list.push(notationHandler);
+    return true;
+  }
+
+  static find(value = this.default) {
+    const notationHandler = value?.constructor === NotationHandler ? value : this.#list.find(x => value === x.name);
+
+    return notationHandler;
+  }
+
+  static #default;
+
+  static set default(value) {
+    if (value.constructor !== NotationHandler) {
+      throw new TypeError(`Invalid notation handler was set as default`);
+    }
+    this.#default = value;
+  }
+  static get default() {
+    return this.#default;
+  }
+}
+
+const JSON_NOTATION = new NotationHandler('json');
+JSON_NOTATION.parseMethod = JSON.parse;
+JSON_NOTATION.stringifyMethod = JSON.stringify;
+NotationHandler.default = JSON_NOTATION;
 
 // The default safe length of a single property
 const DEFAULT_SAFE_LENGTH = 500;
 
 /**
  * Constructor that extends the in-built Map
- * 
+ *
  * [Static properties]
  * - All properties from Map
- * 
+ *
  * [Static methods]
  * - All methods from Map
- * 
+ *
  * [Instance properties]
  * - All properties from Map.prototype
- * 
+ *
  * [Instance methods]
  * - All methods from Map.prototype
  * - CloneableMap.prototype.clone(key)
@@ -262,21 +318,19 @@ class CloneableMap extends Map {
   clone(key) {
     try {
       return JSON.parse(JSON.stringify( this.get(key) ));
-    } catch (err) {
-      console.error('Failed to deep clone a property');
-    }
+    } catch (err) { }
   }
 }
 
 /**
  * Class for managing data storage and retrieval with optional compression and object notation definition.
- * 
+ *
  * [Instance properties]
  * - DataStorage.prototype.target (read-only)
  *   - Gets the target of this DataStorage instance
  * - DataStorage.prototype.compressor
  *   - Get or set the compressor to be used
- * - DataStorage.prototype.objectNotation
+ * - DataStorage.prototype.notationHandler
  *   - Get or set the object notation to be used
  * - DataStorage.prototype.safeLength
  *   - Get or set the safe length of how many characters can be stored
@@ -295,11 +349,11 @@ class CloneableMap extends Map {
  *   - Get the total size in bytes of dynamic properties
  */
 export class DataStorage {
-  #saving = false;
-  #forcedSave = false;
+  #asynchronousSave = false;
+  #synchronousSave = false;
   #target;
   #compressor;
-  #objectNotation;
+  #notationHandler;
   #safeLength;
   #loaded = false;
   /**
@@ -307,15 +361,18 @@ export class DataStorage {
    * @param {World|Entity|ItemStack} target The target where the properties should be modified
    */
   constructor(target) {
+    if (!target.setDynamicProperty || !target.getDynamicProperty) {
+      throw new TypeError(`DataStorage's 'target' must have support for dynamic properties, but ${target.constructor.name} doesn't provide said support`);
+    }
     this.#target = target;
     this.compressor = Compressor.default;
-    this.objectNotation = ObjectNotation.default;
+    this.notationHandler = NotationHandler.default;
     this.safeLength = DEFAULT_SAFE_LENGTH;
     Object.defineProperty(this, 'data', {
       value: new CloneableMap(),
     });
   }
-  
+
   /**
    * Gets the target where the properties are being written/read
    * @returns {World|Entity|ItemStack} The target of the interactions
@@ -323,7 +380,7 @@ export class DataStorage {
   get target() {
     return this.#target;
   }
-  
+
   /**
    * Sets a compressor to compress the data
    * @param {Compressor|String} value The compressor to be used
@@ -331,7 +388,7 @@ export class DataStorage {
   set compressor(value) {
     const compressor = Compressor.find(value);
     if ( !compressor ) {
-      throw new TypeError(`No valid compressor was provided to ${this.constructor.name}.prototype.compressor`);
+      throw new TypeError(`No valid compressor was provided to 'DataStorage.prototype.compressor'`);
     }
     this.#compressor = compressor;
   }
@@ -342,28 +399,26 @@ export class DataStorage {
   get compressor() {
     return this.#compressor;
   }
-  
+
   /**
    * Sets an object notation to parse and stringify data
    * @param {Object} value The object notation object
    */
-  set objectNotation(value) {
-    if (value?.constructor !== Object) {
-      throw new TypeError(`'DataStorage.prototype.objectNotation' should be of type Object, but instead ${value?.constructor.name} was found`);
+  set notationHandler(value) {
+    const notationHandler = NotationHandler.find(value);
+    if ( !notationHandler ) {
+      throw new TypeError(`No valid notation handler was provided to 'DataStorage.prototype.notationHandler(value)'`);
     }
-    if (value.parse?.constructor !== Function || value.stringify?.constructor !== Function) {
-      throw new SyntaxError(`Invalid ObjectNotation was found. parse/stringify functions are missing`);
-    }
-    this.#objectNotation = value;
+    this.#notationHandler = value;
   }
   /**
    * Get the current object notation in use
    * @returns {Object} The object notation currently in use
    */
-  get objectNotation() {
-    return this.#objectNotation;
+  get notationHandler() {
+    return this.#notationHandler;
   }
-  
+
   /**
    * Sets the safe length that can be stored to dynamic properties
    * @param {Number} value The safe length of dynamic properties
@@ -381,29 +436,37 @@ export class DataStorage {
   get safeLength() {
     return this.#safeLength;
   }
-  
+
   /**
    * Saves data from ClonableMap to dynamic properties
-   * @param {Boolean} force Forces the data to be saved synchronously, and ignores any non-forced save process already ongoing
    */
-  save(force = false) {
-    if (!force && this.#saving || this.#forcedSave) return;
-    
-    if (force) {
-      this.#forcedSave = true;
-      for (const [key, value] of this.data.entries()) {
-        const data = this.compressor.compress(this.objectNotation.stringify(value));
+  save() {
+    if (this.#synchronousSave) return;
+
+    this.#synchronousSave = true;
+    for (const [key, value] of this.data.entries()) {
+      try {
+        const data = this.compressor.compress(this.notationHandler.stringify(value));
         this.#saveRaw(key, data);
+      } catch (err) {
+        console.error(err);
       }
-      this.#forcedSave = false;
-    } else {
-      this.#saving = true;
-      smoothLoop((value, key) => {
-        if (this.#forcedSave) return false;
-        const data = this.compressor.compress(this.objectNotation.stringify(value));
-        this.#saveRaw(key, data);
-      }, this.data).finally(() => this.#saving = false);
     }
+    this.#synchronousSave = false;
+  }
+
+  /**
+   * Saves data from ClonableMap to dynamic properties asynchronously
+   */
+  saveAsync() {
+    this.#asynchronousSave = true;
+    smoothLoop((value, key) => {
+      if (this.#synchronousSave) return new BreakLoop('Synchronous save process started', 2);
+      const data = this.compressor.compress(this.notationHandler.stringify(value));
+      this.#saveRaw(key, data);
+    }, this.data)
+      .catch(console.error)
+      .finally(() => this.#asynchronousSave = false);
   }
   /**
    * Saves raw data to dynamic property
@@ -421,7 +484,7 @@ export class DataStorage {
       console.error(error);
     }
   }
-  
+
   /**
    * Loads all data to ClonableMap
    * @returns {ClonableMap} The ClonableMap assigned to this instance
@@ -429,18 +492,19 @@ export class DataStorage {
   load() {
     if (!this.#loaded) {
       const keys = this.target.getDynamicPropertyIds();
-      
-      for (const key of keys) {
+
+      for (let index = 0; index < keys.length; index++) {
+        const key = keys[index];
         const value = this.#loadRaw(key);
         try {
           const data = this.compressor.decompress(value);
-          
-          this.data.set(key, this.objectNotation.parse(data));
+
+          this.data.set(key, this.notationHandler.parse(data));
         } catch(error) {
           console.error(error);
         }
       }
-      
+
       this.#loaded = true;
     }
     return this.data;
@@ -453,7 +517,7 @@ export class DataStorage {
   #loadRaw(key) {
     return this.target.getDynamicProperty(key);
   }
-  
+
   /**
    * Removes data from a dynamic property and also from the ClonableMap
    * @param {String} key The key where the data should be deleted from
@@ -467,7 +531,7 @@ export class DataStorage {
     }
     return false;
   }
-  
+
   /**
    * Clear all data from dynamic properties and ClonableMap
    */
@@ -475,7 +539,7 @@ export class DataStorage {
     this.data.clear();
     this.target.clearDynamicProperties();
   }
-  
+
   /**
    * Get the length of the string assigned to key
    * @param {String} key The key where to check the length
@@ -484,7 +548,7 @@ export class DataStorage {
   length(key) {
     return this.#loadRaw(key)?.length || 0;
   }
-  
+
   /**
    * Get the total size of dynamic properties
    * @returns {Number} The total size of all dynamic properties combined
@@ -505,8 +569,8 @@ const property = {
   },
 }
 
-if (!world.dataStorage) {
-  Object.defineProperty(world, 'dataStorage', property);
+if (!World.prototype.dataStorage) {
+  Object.defineProperty(World.prototype, 'dataStorage', property);
 }
 
 if (!Entity.prototype.dataStorage) {
